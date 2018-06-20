02568c597967c749372ffe7c5cecf7b39f1b9db0
OssFate <mantis1512@gmail.com>
2018-06-19 20:30:00 -0500

    Fruit spawn and pick Yep, things to do:   1. Wall hacks   2. Make it HUUGGEEE!!   3. Make it prettier

--- Gusanito/Game.gd
+++ Gusanito/Game.gd
@@ -1,34 +1,43 @@
 extends Node
 
+var projectResolution = OS.get_window_safe_area().size
+var fruitOffset = 32
+
 var snake = load("res://snake.tscn")
 var fruit = load("res://Fruit.tscn")
-var seconds = 0;
-onready var timer = get_node("Label")
+var seconds = 0
+onready var timer = get_node("Timer")
 
 var tail = []
 
 func _ready():
 	tail.append(snake.instance())
+	#print(projectResolution)
 	
 	add_child(tail[0])
+	spawn_fruit()
 	pass
 
-func spawn_fruits():
-	var projectResolution = OS.get_real_window_size()
-	
+func spawn_fruit():
 	var node = fruit.instance()
-	var newPos = Vector2(rand_range(0, projectResolution.x), rand_range(0, projectResolution.y))
+	var newPos = Vector2(rand_range(fruitOffset, projectResolution.x - fruitOffset), rand_range(fruitOffset, projectResolution.y - fruitOffset))
 	node.position = newPos
+	#print(newPos)
+	add_child(node)
+	pass
+
+func spawn_fruit_at(pos):
+	var node = fruit.instance()
+	node.position = pos
 	
 	add_child(node)
-	
 	pass
 
 func _on_Spawn_button_up():
-	spawn_fruits()
+	#spawn_fruit()
 	pass # replace with function body
 
 func _process(delta):
-	seconds += delta;
+	seconds += delta
 	timer.set_text("tiempo %d segundos" % seconds)
 	pass
--- Gusanito/snake.gd
+++ Gusanito/snake.gd
@@ -1,5 +1,7 @@
 extends KinematicBody2D
 
+const gameInstance = preload("res://Game.gd")
+
 var motion = Vector2()
 export var speed = 100
 
@@ -21,3 +23,14 @@ func _physics_process(delta):
 		motion.y = speed
 	
 	move_and_slide(motion)
+	
+	if get_slide_count() > 0 :
+		var coll = get_slide_collision(0)
+		coll.collider.queue_free()
+		get_bigger()
+		#re spawn fruit
+		get_node("..").spawn_fruit()
+
+func get_bigger():
+	#Should get big here :D
+	print("ITS GONNA BE HUUUUGGGEEEEE")
